c2cc18eef065ec66dcb7b163001e4615efa11b81
jivi3 <jivirockz1@gmail.com>
2020-04-25 14:32:46 -0400

    fixed heroku

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ router.get("/",function(req, res){
 });
 
 router.get("/home",function(req, res){
-  res.sendFIle(path + "home.html");
+  res.sendFile(path + "home.html");
 });
 
 router.get("/business",function(req, res){
